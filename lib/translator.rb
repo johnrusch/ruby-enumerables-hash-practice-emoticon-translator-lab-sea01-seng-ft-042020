37d4c075dcdf4c651cd4deb28c4c935392970178
John Rusch <johnrusch7@gmail.com>
2020-04-01 21:55:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,6 @@ def get_english_meaning(library, emoticon)
   if library_hash.include?(emoticon)
     translation = library_hash.key(emoticon)
   end
-  #binding.pry 
+  binding.pry 
   translation
 end
